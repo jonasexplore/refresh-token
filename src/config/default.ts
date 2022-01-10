0522f5ae94282011e2880e1b0fc19ad530828b82
Jonas Brito <jonasexplore@gmail.com>
2022-01-10 11:56:52 -0300

    chore(config): add more variables

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,4 +1,7 @@
 export const PORT = process.env.PORT || 3333;
 export const SALT = 10;
 export const JWT_SECRET = "HJASDAH&@N*(!56231ASD";
+export const JWT_EXPIRE_TOKEN = "12000ms"; // 12 seconds
 export const JWT_REFRESH_SECRET = "ASDA&*@!3741jSKAD";
+export const JWT_EXPIRE_REFRESH_TOKEN = "7d"; // 7 days
+export const REDIS_EXPIRE_REFRESH_TOKEN = 604800; // 7 days
