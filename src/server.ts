b9cf14831791b1c7a3ef6c72687e697075ba1cbf
Jonas Brito <jonasexplore@gmail.com>
2022-01-10 11:59:22 -0300

    chore(*): add interceptor execeptions

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
+import "express-async-errors";
+
 import express from "express";
 
 import { PORT } from "./config/default";
 import { routes } from "./routes";
+import { interceptExceptions } from "./infra/interceptors";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(interceptExceptions);
 
 app.listen(PORT, () =>
   console.log(`🚀 Server has started at http://localhost:${PORT}`)
